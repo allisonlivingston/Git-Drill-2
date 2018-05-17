b6c601b15ad8ea5a78cd2dbfeb616ef4a7177a96
Christopher Nemeth <cnemeth1@gmail.com>
2018-05-17 11:30:54 -0600

    added an array of numbers and a function that returns the even numbers

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ var people = [
     age: 30
   },
 ]
+var arr = [4, 5, 7, 8, 14, 45, 76];
+
+function even(a) {
+  return a.filter(function (val) {
+    return val % 2 === 0
+  })
+}
+
+alert(even(arr));
